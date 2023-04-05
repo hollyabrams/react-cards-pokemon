0d5c1630b584e0b2f1030e2e50cf59608f7ef407
Holly <holly.d.abrams@gmail.com>
2023-04-05 12:45:37 -0400

    Added custom hooks

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,18 +1,15 @@
-import { useState } from "react";
 import backOfCard from "./back.png";
-import "./PlayingCard.css"
+import { useFlip } from "./hooks/useFlip";
+import "./PlayingCard.css";
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
+  const [isFacingUp, flip] = useFlip();
   return (
     <img
       src={isFacingUp ? front : back}
       alt="playing card"
-      onClick={flipCard}
+      onClick={flip}
       className="PlayingCard Card"
     />
   );
